59bdc33d52410305104d61e596b0911566228b3f
kenllah <kenllah@yahoo.com>
2025-01-07 03:22:37 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import image1 from './public/image1.svg';
-import image2 from './public/image2.svg';
-import image3 from './public/image3.svg';
+import Image from 'next/image'; // Import the Next.js Image component
+import image1 from './image1.svg';
+import image2 from './image2.svg';
+import image3 from './image3.svg';
 
 const HazmatTutor = () => {
   const [currentSection, setCurrentSection] = useState(0);
