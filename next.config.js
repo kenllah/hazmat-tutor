76a72770e7fc65f3581dfa6cebd483e28b8a128c
kenllah <kenllah@yahoo.com>
2025-01-07 14:31:22 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+// next.config.js
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/favicon.ico',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
+};
